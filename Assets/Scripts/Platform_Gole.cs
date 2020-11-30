123081ccbaa2c9ca31a456c1a1a9516da84f2f43
StupidReadHuang <aqblwd@outlook.com>
2020-12-01 03:26:48 +0800

    添加DateNight项目

---
+++ Assets/Scripts/Platform_Gole.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Gole : MonoBehaviour
+{
+
+    public bool hasBlue;
+    public bool hasPink;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player_BottomTrigger")) {
+            string parentTag = collision.transform.parent.tag;
+            if (parentTag == "Player_Blue")
+                hasBlue = false;
+            else if (parentTag == "Player_Pink")
+                hasPink = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player_BottomTrigger"))
+        {
+            string parentTag = collision.transform.parent.tag;
+            if (parentTag == "Player_Blue")
+                hasBlue = true;
+            else if (parentTag == "Player_Pink")
+                hasPink = true;
+        }
+        if (hasPink && hasBlue)
+            GameManager.instance.CompleteLevel();
+    }
+}
